27b5abb21414997e6f8a3772ca4b8b6e0d75a4c2
Naim <naimahmed.srj@gmail.com>
2024-10-25 07:28:40 +0600

    chapter_2

---
+++ Chapter 2/01_variables.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int i = 10;
+    int j = i;
+    int a = 2, b = 3, c = 4, d = 5;
+    printf("print the value of i is %d, j is %d\n", i, j);
+    printf("print the value of a is %d, b is %d\n", a, b);
+    printf("print the value of c is %d, d is %d\n", c, d);
+    return 0;
+}
