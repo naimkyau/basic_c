b7f051589c92efc7d6ecae308d1bf92de2224f01
Naim <naimahmed.srj@gmail.com>
2024-10-26 09:31:52 +0600

    decrementing for loop

---
+++ Chapter 4/07_quick_quiz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+
+    int n, i = 1;;
+
+    printf("Input the value of n :");
+    scanf("%d", &n);
+
+    do {
+        printf("%d\n",i);
+        i++;
+    } while (i <= n);
+    
+    return 0;
+}
