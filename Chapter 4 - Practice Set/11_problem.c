07fb761ea095fc0156b78083419ea19c75975e8d
Naim <naimahmed.srj@gmail.com>
2024-10-27 20:28:20 +0600

    chapter 4 problem all solved

---
+++ Chapter 4 - Practice Set/11_problem.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+
+    if (n <= 1) {
+        printf("%d NOT a prime number\n", n);
+    } else {
+        int is_prime = 1; // Assume n is prime until proven otherwise
+        i = 2;
+
+        while (i * i <= n) {
+            if (n % i == 0) {
+                is_prime = 0; // n is not prime
+                break;
+            }
+            i++;
+        }
+
+        if (is_prime) {
+            printf("%d IS a prime number\n", n);
+        } else {
+            printf("%d NOT a prime number\n", n);
+        }
+    }
+
+    return 0;
+}
--- Chapter 4 - Practice Set/12_problem.c
+++ Chapter 4 - Practice Set/12_problem.c
@@ -5,19 +5,20 @@ int main() {
     printf("Enter the value of n: ");
     scanf("%d", &n);
 
-    // Check if n is less than or equal to 1
     if (n <= 1) {
         printf("%d NOT a prime number\n", n);
     } else {
         int is_prime = 1; // Assume n is prime until proven otherwise
-        
-        for (i = 2; i * i <= n; i++) { // Check up to the square root of n
+        i = 2;
+
+        do {
             if (n % i == 0) {
                 is_prime = 0; // n is not prime
                 break;
             }
-        }
-        
+            i++;
+        } while (i * i <= n);
+
         if (is_prime) {
             printf("%d IS a prime number\n", n);
         } else {
