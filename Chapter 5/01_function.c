5ccc78af24f53cad09fa91abef4959dc8c55357c
Naim <naimahmed.srj@gmail.com>
2024-10-29 18:12:37 +0600

    function

--- Chapter 5/01_function.c
+++ Chapter 5/01_function.c
@@ -1,10 +1,25 @@
 #include <stdio.h>
 
-int main() {
-    int a=2;
-    int b=3;
+int sum(int x, int y){
+    printf("The sum is %d\n", x+y);
+    return x+y;
+}
 
+int main(){
+    int a1 = 2;
+    int b1 = 3;
 
-    
+    sum(a1,b1); 
+
+    int a2 = 4;
+    int b2 = 5;
+
+    sum(a2,b2);
+
+    int a3 = 7;
+    int b3 = 8;
+
+    sum(a3, b3);
+
     return 0;
 }
