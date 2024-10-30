5e6d83a1bb747f2cb159db88e15fefd302ed50bf
Naim <naimahmed.srj@gmail.com>
2024-10-30 15:36:28 +0600

    quick quiz

---
+++ Chapter 5/04_quick_quiz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    double side = 5;
+    double area;
+
+    area = pow(side, 2);
+
+    printf("The area is %.3lf\n", area);
+
+
+    return 0;
+}
