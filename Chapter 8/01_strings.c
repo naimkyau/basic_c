2ea7f4a9509486e6c5e1c4fc776bac3e60db8f22
Naim <naimahmed.srj@gmail.com>
2024-11-05 07:18:05 +0600

    strings

---
+++ Chapter 8/01_strings.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    char name1[] = {'N', 'A', 'I', 'M', '\0'};
+    char name2[] = "NAIM";
+
+    for (int i = 0; i < 4; i++)
+    {
+        printf("%c", name1[i]);
+    }
+    printf("\n");
+    for (int i = 0; i < 4; i++)
+    {
+        printf("%c", name2[i]);
+    }
+
+    return 0;
+}
