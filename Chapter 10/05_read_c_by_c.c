86f62fcb9ab2569afa842e1e3c0350b7e3d458ab
Muhammad Naim <naimahmed.srj@gmail.com>
2024-11-11 17:16:40 +0600

    File read char by char

---
+++ Chapter 10/05_read_c_by_c.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+    char ch;
+    FILE *ptr;
+    ptr = fopen("naim.txt", "r");
+    
+    while (1){
+        ch = fgetc(ptr);
+        printf("%c", ch);
+        if (ch ==EOF){
+            break;
+        }
+    }
+
+    return 0;
+}
